a6f952a7428d4e3bd2da4d5c93bd67c54c132c49
Gabriel Mercês <67835914+diGit-hub@users.noreply.github.com>
2026-04-08 16:22:23 -0300

    add AdminProducts component and .env configuration

---
+++ src/pages/AdminProducts.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export default function AdminProducts() {
+    const [productos, setProductos] = useState([]);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        
+    })
+    return(
+        <>
+
+        </>
+    )
+}
